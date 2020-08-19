25bf09ff0f25e393dab49773e3a3fad2cfd891f1
Nguyễn Ngọc Cường <ChetDuoiKhiDanhRang@users.noreply.github.com>
2020-08-19 12:40:47 +0700

    setup

--- mSim/graphicForm.Designer.cs
+++ mSim/graphicForm.Designer.cs
@@ -984,7 +984,7 @@
             this.MinimumSize = new System.Drawing.Size(1024, 768);
             this.Name = "drawForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "mSim (DEMO)";
+            this.Text = "Motion Simulator";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.drawForm_FormClosed);
             this.Shown += new System.EventHandler(this.drawForm_Shown);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.drawForm_KeyDown);
